64bc51f8cd0836625b95d5ece5a8784741d0826f
Arthur Ngondo <ngondoarthur@gmail.com>
2017-06-15 09:14:12 +0300

    Minimal 3 day forecast.....To Be..Continued

--- wu_weather.py
+++ wu_weather.py
@@ -6,12 +6,30 @@ import requests
 import sys
 
 # Compute location from command line arguments
+# If there is only one commandline argument, provide usage lines
 if len(sys.argv) < 2:
     print('Usage: wu_weather.py location')
     sys.exit()
 location = ' '.join(sys.argv[1: ])
 
 # TODO: Download the JSON data from OpenWeatherMap.org's API
+url = 'http://api.openweathermap.org/data/2.5/forecast/daily?' + \
+        'q={}&appid=ed613986d76fecf30a2a48f71f8de026'.format(location)
+# Assign object to *response* and Confirm data is downloaded ok       
+response = requests.get(url)
+response.raise_for_status()
 
 # TODO: LOAD JSON data into python variable
+weather_data = json.loads(response.text)
 
+#Print weather descriptions.
+
+w = weather_data['list']
+print('Current weather in {}:'.format(location))
+print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+print()
+print('Tomorrow:')
+print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+print()
+print('Day after tomorrow:')
+print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
